45c178fbd4967172c0ad4a0c115b3f271c554b30
Mike Larsson <mikelarssonftw@gmail.com>
2013-07-19 14:04:00 -0400

    Add more template variables

--- test/app.test.js
+++ test/app.test.js
@@ -76,35 +76,73 @@ describe('App', function(){
 
 });
 
-describe('`test-1.0.json` assertion', function() {
+describe('Assertion templating', function() {
 
-  it('badge url with {{ server }} should be local to app', function(done) {
-    request(app)
-      .get('/test.json')
-      .expect(200, function(err, res){
-        if (err)
-          return done(err);
-        res.body.badge.should.exist;
-        res.body.badge.should.include(res.req._headers.host);
-        request(res.body.badge)
-          .get('')
-          .expect(200)
-          .expect('Content-Type', /json/, done);
-      });
+  describe('`test.json`', function() {
+
+    it('badge.json url should be local to app', function(done) {
+      request(app)
+        .get('/test.json')
+        .expect(200, function(err, res){
+          if (err)
+            return done(err);
+          res.body.badge.should.exist;
+          res.body.badge.should.include(res.req._headers.host);
+          request(res.body.badge)
+            .get('')
+            .expect(200)
+            .expect('Content-Type', /json/, done);
+        });
+    });
+
+    it('verify.url should be self', function(done) {
+      request(app)
+        .get('/test.json')
+        .expect(200, function(err, res){
+          if (err)
+            return done(err);
+          res.body.verify.should.exist;
+          res.body.verify.url.should.exist;
+          /* FIXME: Is there a better way to get original request url? */
+          res.body.verify.url.should.include(res.req._headers.host);
+          res.body.verify.url.should.include(res.req.path);
+          done();
+        });
+    });
   });
 
-  it('verify url with {{ self }} should be local to app', function(done) {
-    request(app)
-      .get('/test.json')
-      .expect(200, function(err, res){
-        if (err)
-          return done(err);
-        res.body.verify.should.exist;
-        res.body.verify.url.should.exist;
-        /* FIXME: Is there a better way to get original request url? */
-        res.body.verify.url.should.include(res.req._headers.host);
-        res.body.verify.url.should.include(res.req.path);
-        done();
-      });
+  describe('`badge.json`', function() {
+    
+    it('badge image url should be local to app', function(done) {
+      request(app)
+        .get('/badge.json')
+        .expect(200, function(err, res){
+          if (err)
+            return done(err);
+          res.body.image.should.exist;
+          res.body.image.should.include(res.req._headers.host);
+          request(res.body.image)
+            .get('')
+            .expect(200)
+            .expect('Content-Type', /png/, done);
+        });
+    });
+
+  });
+
+  describe('`issuer.json`', function() {
+    
+    it('issuer url should be app base url', function(done) {
+      request(app)
+        .get('/issuer.json')
+        .expect(200, function(err, res){
+          if (err)
+            return done(err);
+          res.body.url.should.exist;
+          res.body.url.should.equal('http://' + res.req._headers.host);
+          done();
+        });
+    });
+
   });
 });
