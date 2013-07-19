92c97a4ccbb0c7d3c10352d0c571a1fa039cfaa1
Mike Larsson <mikelarssonftw@gmail.com>
2013-07-19 17:48:43 -0400

    Add simple ?set={} and ?merge={} functionality. Doesn't yet handle setting or merging values in 1.0 objects referenced by url (badge.json, issuer.json). Doesn't yet work for versioned paths.

--- test/app.test.js
+++ test/app.test.js
@@ -9,20 +9,16 @@ var app = require('../').app.build({
 app.listen(0);
 
 describe('App', function(){
-
   describe('static assets', function(){
-
     it('should host static assets', function(done) {
       request(app)
         .get('/badge.png')
         .expect(200)
         .expect('Content-Type', /png/, done);
     });
-
   });
 
   describe('/xxx.json', function(){
-
     ['test', 'badge', 'issuer'].forEach(function(name) {
       var path = '/' + name + '.json';
       it('should host assertion file ' + path, function(done) { 
@@ -38,11 +34,9 @@ describe('App', function(){
           .get('/NOPE.json')
           .expect(404, done)
     });
-
   });
 
   describe('/0.5/xxx.json', function(){
-    
     it('should return assertion if it\'s 0.5', function(done) {
       request(app)
         .get('/0.5/test-0.5.json')
@@ -58,7 +52,6 @@ describe('App', function(){
   });
 
   describe('/1.0/xxx.json', function(){
-
     it('should return assertion if it\'s 1.0', function(done) {
       request(app)
         .get('/1.0/test-1.0.json')
@@ -71,15 +64,67 @@ describe('App', function(){
         .get('/1.0/test-0.5.json')
         .expect(409, done);
     });
-
   });
 
 });
 
+describe('?set={...}', function(){
+  it('should set assertion values', function(done){
+    var obj = encodeURIComponent(JSON.stringify({
+      foo: 'bar',
+      recipient: {
+        foo: 'bar'
+      },
+      baz: {
+        foo: 'bar'
+      }
+    }));
+    request(app)
+      .get('/test.json?set=' + obj)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err)
+          return done(err);
+        res.body.should.have.property('foo', 'bar');
+        res.body.should.have.property('recipient');
+        res.body.recipient.should.eql({ 'foo': 'bar' });
+        res.body.should.have.property('baz');
+        res.body.baz.should.eql({ 'foo': 'bar' });
+        done();
+      });
+  });
+
+  it('should merge assertion values', function(done){
+    var obj = encodeURIComponent(JSON.stringify({
+      foo: 'bar',
+      recipient: {
+        foo: 'bar'
+      },
+      baz: {
+        foo: 'bar'
+      }
+    }));
+    request(app)
+      .get('/test.json?merge=' + obj)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err)
+          return done(err);
+        res.body.should.have.property('foo', 'bar');
+        res.body.should.have.property('recipient');
+        res.body.recipient.should.have.property('foo', 'bar');
+        res.body.recipient.should.have.property('hashed'); // pre-existing in test.json
+        res.body.should.have.property('baz');
+        res.body.baz.should.eql({ 'foo': 'bar' });
+        done();
+      });
+  });
+});
+
 describe('Assertion templating', function() {
-
   describe('`test.json`', function() {
-
     it('badge.json url should be local to app', function(done) {
       request(app)
         .get('/test.json')
@@ -112,7 +157,6 @@ describe('Assertion templating', function() {
   });
 
   describe('`badge.json`', function() {
-    
     it('badge image url should be local to app', function(done) {
       request(app)
         .get('/badge.json')
@@ -127,11 +171,9 @@ describe('Assertion templating', function() {
             .expect('Content-Type', /png/, done);
         });
     });
-
   });
 
   describe('`issuer.json`', function() {
-    
     it('issuer url should be app base url', function(done) {
       request(app)
         .get('/issuer.json')
@@ -143,6 +185,5 @@ describe('Assertion templating', function() {
           done();
         });
     });
-
   });
 });
